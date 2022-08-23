b193b7d62059c8129774f5d7219a844be20eac78
YiLiu6240 <yi.liu6240@gmail.com>
2022-08-23 16:36:58 +0100

    file reader

--- annotator/src/views/Home.vue
+++ annotator/src/views/Home.vue
@@ -1,4 +1,6 @@
-<template lang="pug"></template>
+<template lang="pug">
+v-btn(@click="populateContent") Load data
+</template>
 
 <script lang="ts">
 import Vue from "vue";
@@ -10,14 +12,40 @@ export default Vue.extend({
   },
   data() {
     return {
-      //
+      fileContent: null,
+      foobar: "foo--bar",
     };
   },
   computed: {
     //
   },
   methods: {
-    //
+    async populateContent(): Promise<any> {
+      const fileInfo = await this.getFile();
+      let reader = new FileReader();
+      reader.readAsText(fileInfo);
+      reader.onload = () => {
+        this.fileContent = JSON.parse(reader.result as string);
+      };
+    },
+    async getFile(): Promise<any> {
+      const pickerOpts = {
+        types: [
+          {
+            description: "result file",
+            accept: {
+              "application/*": [".json"],
+            },
+            excludeAcceptAllOption: true,
+            multiple: false,
+          },
+        ],
+      };
+      var fileHandle;
+      [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+      const fileInfo = await fileHandle.getFile();
+      return fileInfo;
+    },
   },
 });
 </script>
