111c8f3dc50dfe8f810ae902d7f34f22e7393d67
YiLiu6240 <yi.liu6240@gmail.com>
2022-08-23 17:29:02 +0100

    file writer

--- annotator/src/views/Home.vue
+++ annotator/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-v-btn(@click="populateContent") Load data
+v-container
+  v-btn(@click="specifyInput") Specify Input file
+  v-btn(@click="specifyOutput") Specify output file
 </template>
 
 <script lang="ts">
@@ -20,19 +22,29 @@ export default Vue.extend({
     //
   },
   methods: {
-    async populateContent(): Promise<any> {
-      const fileInfo = await this.getFile();
+    async specifyInput(): Promise<any> {
+      const fileInfo = await this.getInputFile();
+      console.log(fileInfo);
+      console.log(fileInfo.name);
       let reader = new FileReader();
       reader.readAsText(fileInfo);
       reader.onload = () => {
         this.fileContent = JSON.parse(reader.result as string);
       };
     },
-    async getFile(): Promise<any> {
+    async specifyOutput(): Promise<any> {
+      const fileInfo = await this.getOutputFile();
+      console.log(fileInfo);
+      console.log(fileInfo.name);
+      const writableStream = await fileInfo.createWritable();
+      await writableStream.write(this.foobar);
+      await writableStream.close();
+    },
+    async getInputFile(): Promise<any> {
       const pickerOpts = {
         types: [
           {
-            description: "result file",
+            description: "JSON file",
             accept: {
               "application/*": [".json"],
             },
@@ -45,6 +57,20 @@ export default Vue.extend({
       [fileHandle] = await window.showOpenFilePicker(pickerOpts);
       const fileInfo = await fileHandle.getFile();
       return fileInfo;
+    },
+    async getOutputFile(): Promise<any> {
+      const pickerOpts = {
+        suggestedName: "annotation_results.json",
+        types: [
+          {
+            description: "JSON file",
+            accept: {
+              "application/*": [".json"],
+            },
+          },
+        ],
+      };
+      return await window.showSaveFilePicker(pickerOpts);
     },
   },
 });
