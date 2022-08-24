5e5501f31971444df1de6fca4e14e54672ffacb4
YiLiu6240 <yi.liu6240@gmail.com>
2022-08-24 17:29:12 +0100

    data iterator

--- annotator/src/views/Home.vue
+++ annotator/src/views/Home.vue
@@ -2,22 +2,28 @@
 v-container
   IO
   .py-5
+  div(v-if="showTable")
+    data-table
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import IO from "@/components/IO.vue";
+import DataTable from "@/components/DataTable.vue";
 
 export default Vue.extend({
   name: "Home",
   components: {
     IO,
+    DataTable,
   },
   data() {
     return {};
   },
   computed: {
-    //
+    showTable(): boolean {
+      return this.$store.state.annotationData.data != null ? true : false;
+    },
   },
   methods: {},
 });
