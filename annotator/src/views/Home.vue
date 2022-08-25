cb75b5e92eaadd963e9b383da9ae6d18e5c9693b
YiLiu6240 <yi.liu6240@gmail.com>
2022-08-25 19:29:09 +0100

    Make data items searchable

--- annotator/src/views/Home.vue
+++ annotator/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template lang="pug">
-v-container(fluid)
+v-container
+  h1 File settings
   IO
   .py-5
+  h1 Metadata settings
+  div
+    span Flags / tags for query items
+    br
+    span Flags / tags for mapping items
+  .py-5
+  h1 Annotation for mapping results
   div(v-if="showTable")
     data-table
     v-btn.floating-button(color="success", x-large)
@@ -35,8 +43,8 @@ export default Vue.extend({
 <style scoped>
 .floating-button {
   position: fixed;
-  right: 150px;
-  bottom: 40px;
+  right: 20px;
+  bottom: 20px;
   z-index: 120;
 }
 </style>
