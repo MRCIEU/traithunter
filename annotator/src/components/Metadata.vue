2020a0a987a57322e7c259467f271793cc3d58b6
YiLiu6240 <yi.liu6240@gmail.com>
2022-08-26 13:05:23 +0100

    Metadata flags

---
+++ annotator/src/components/Metadata.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+v-container
+  metadata-flags
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import MetadataFlags from "@/components/widgets/MetadataFlags.vue";
+
+export default Vue.extend({
+  name: "Metadata",
+  components: {
+    MetadataFlags,
+  },
+  data() {
+    return {
+      //
+    };
+  },
+  computed: {
+    //
+  },
+  methods: {
+    //
+  },
+});
+</script>
--- annotator/src/views/Home.vue
+++ annotator/src/views/Home.vue
@@ -4,13 +4,11 @@ v-container
   IO
   .py-5
   h1 Metadata settings
-  div
-    span Flags / tags for query items
-    br
-    span Flags / tags for mapping items
+  div(v-if="ioDone")
+    metadata
   .py-5
   h1 Annotation for mapping results
-  div(v-if="showTable")
+  div(v-if="ioDone")
     data-table
     v-btn.floating-button(color="success", x-large)
       v-icon mdi-content-save
@@ -21,18 +19,20 @@ v-container
 import Vue from "vue";
 import IO from "@/components/IO.vue";
 import DataTable from "@/components/DataTable.vue";
+import Metadata from "@/components/Metadata.vue";
 
 export default Vue.extend({
   name: "Home",
   components: {
     IO,
     DataTable,
+    Metadata,
   },
   data() {
     return {};
   },
   computed: {
-    showTable(): boolean {
+    ioDone(): boolean {
       return this.$store.state.annotationData.data != null ? true : false;
     },
   },
