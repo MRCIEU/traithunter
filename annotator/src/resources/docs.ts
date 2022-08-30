6b95828eb3518851d51da7cca8a0edea2f5d0b35
YiLiu6240 <yi.liu6240@gmail.com>
2022-08-30 19:49:55 +0100

    Placeholder for docs

--- annotator/src/resources/docs.ts
+++ annotator/src/resources/docs.ts
@@ -24,3 +24,37 @@ export const inputConfig = `
 
 export const btnSave = "Save your progress";
 export const btnExport = "Export a flat JSON file from the annotation data";
+
+export const docsAbout = `
+## Annotator for phenotype harmonization
+
+This is a companion app for annotating phenotype mapping / harmonization results produced by the \`phenotype mapper\`.
+`;
+
+export const docsBasicUsage = ``;
+
+export const docsFileFormat = `
+
+\`\`\`
+AnnotationDataItem = {
+  trait_id: string;
+  trait_term: string;
+  trait_term_query: Array<string>;
+  trait_basic_info: {
+    phenotype: string;
+    trait_type: string;
+    dataset: string;
+  };
+  equivalence_res: Array<VectorEntItem>;
+  composite_res: Array<VectorEntItem>;
+  candidates: Array<VectorEntItem>;
+  selection: Array<string>;
+  external_selection: Array<BaseEnt>;
+  flags: Array<FlagItem>;
+  notes: string;
+}
+\`\`\`
+
+`;
+
+export const MappingStrategies = ``;
