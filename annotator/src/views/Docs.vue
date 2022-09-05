ef1ec63f625abcdd9bab5826bd1979edcdb858c5
YiLiu6240 <yi.liu6240@gmail.com>
2022-09-05 11:46:41 +0100

    Update docs

--- annotator/src/views/Docs.vue
+++ annotator/src/views/Docs.vue
@@ -1,26 +1,29 @@
 <template lang="pug">
 v-container
-  v-card.py-5
-    v-card-title About
-    v-card-text
-      vue-markdown(:source="docs.docsAbout", :breaks="false")
-  v-divider
-  v-card.py-5
-    v-card-title Basic usage
-    v-card-text
-      vue-markdown(:source="docs.docsBasicUsage", :breaks="false")
-  v-divider
-  v-card.py-5
-    v-card-title Technical specifications
-    v-card-text
-      vue-markdown(:source="docs.docsTechSpecs", :breaks="false")
-  v-divider
-  v-card.py-5
-    v-card-title Types
-    v-card-text
-      vue-markdown(:source="docs.docsTypes", :breaks="false")
-      prism(language="typescript", :code="typeRaw")
-  v-divider
+  v-row
+    v-col(cols="6")
+      v-card.py-5
+        v-card-title About
+        v-card-text
+          vue-markdown(:source="docs.docsAbout", :breaks="false")
+      v-divider
+      v-card.py-5
+        v-card-title Basic usage
+        v-card-text
+          vue-markdown(:source="docs.docsBasicUsage", :breaks="false")
+      v-divider
+      v-card.py-5
+        v-card-title Technical specifications
+        v-card-text
+          vue-markdown(:source="docs.docsTechSpecs", :breaks="false")
+      v-divider
+    v-col(cols="6")
+      v-card.py-5
+        v-card-title Types
+        v-card-text
+          vue-markdown(:source="docs.docsTypes", :breaks="false")
+          prism(language="typescript", :code="typeRaw")
+      v-divider
 </template>
 
 <script lang="ts">
