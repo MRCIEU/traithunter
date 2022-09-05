09e269366924cac94205d52ee7d9383db38f7f0e
YiLiu6240 <yi.liu6240@gmail.com>
2022-09-05 12:52:17 +0100

    Work on api

---
+++ backend/app/graphql_api/definitions/ontology_ent.py
@@ -0,0 +1,9 @@
+# from typing import List
+# import strawberry
+
+# _docs = "Retrieve ontology entity by its `ent_id` or `ent_term`"
+
+
+# @strawberry.type(description=_docs)
+# class OntologyEntQuery:
+#     results: List[OntologyEnt]
