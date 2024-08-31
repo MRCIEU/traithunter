5cba82d50c79424693800b03dc8301a593bc453d
YiLiu6240 <yi.liu6240@gmail.com>
2024-08-31 16:11:27 +0100

    backend: cache

--- backend/app/api/dictionary/__init__.py
+++ backend/app/api/dictionary/__init__.py
@@ -1,8 +1,11 @@
 from typing import List
 
+import requests
 from fastapi import APIRouter
+from fastapi_cache.decorator import cache
 
 from app.resources import es_config
+from app.settings import ES_URL
 
 router = APIRouter()
 
@@ -13,4 +16,14 @@ async def get_entity_dictionary_list() -> List:
     return res
 
 
-# async def get
+@cache()
+@router.get("/entity/dictionary/length")
+async def get_entity_dictionary_length(dictionary: str) -> int:
+    assert dictionary in es_config.DICTIONARY_NAMES
+    dictionary_index = es_config.DICTIONARY_INDICES[dictionary]
+    url = f"{ES_URL}/{dictionary_index}/_stats"
+    r = requests.get(url)
+    r.raise_for_status()
+    response = r.json()
+    res = response["indices"][dictionary_index]["primaries"]["docs"]["count"]
+    return res
--- backend/tests/test_dictionary.py
+++ backend/tests/test_dictionary.py
@@ -10,3 +10,11 @@ def test_entity_dictionary_list():
     r = client.get(url=url)
     r.raise_for_status()
     assert len(r.json()) > 0
+
+
+def test_entity_dictionary_length():
+    url = "/entity/dictionary/length"
+    params = {"dictionary": "hpo"}
+    r = client.get(url=url, params=params)
+    r.raise_for_status()
+    assert r.json() > 0
