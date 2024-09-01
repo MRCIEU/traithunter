67813ad8b39725742ab938db3598d990154ef6ae
YiLiu6240 <yi.liu6240@gmail.com>
2024-09-01 13:35:02 +0100

    wip

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,12 @@
-<template>
-  <v-app>
-    <app-bar />
-    <v-main>
-      <router-view />
-      <snackbar-widget />
-    </v-main>
-    <div v-if="gtagId">
-      <cookie-consent />
-    </div>
-    <app-footer />
-  </v-app>
+<template lang="pug">
+v-app
+  app-bar
+  v-main
+    router-view
+    snackbar-widget
+  app-footer
 </template>
+
 
 <script lang="ts">
 import Vue from "vue";
@@ -30,15 +26,7 @@ export default Vue.extend({
     CookieConsent,
   },
   data: () => ({
-    gtagId: gtagId,
+    //
   }),
 });
 </script>
-
-<style lang="scss">
-/* $cookieconstent-background: $light !default;
-   $cookieconstent-compliance-background: $primary !default; */
-@import "~vue-cookieconsent-component/src/scss/cookie-consent";
-@import "~vue-cookieconsent-component/src/scss/cookie-consent-bottom";
-@import "~vue-cookieconsent-component/src/scss/cookie-consent-transition";
-</style>
