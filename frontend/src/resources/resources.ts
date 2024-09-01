d6b0f12df5600362836f3cfb94b26cdac14f1ef8
YiLiu6240 <yi.liu6240@gmail.com>
2024-09-01 12:40:06 +0100

    wip

--- frontend/src/resources/resources.ts
+++
@@ -1,4 +0,0 @@
-export const semanticTypeUrl =
-  "https://lhncbc.nlm.nih.gov/ii/tools/MetaMap/Docs/SemanticTypes_2018AB.txt";
-export const umlsPredicateUrl =
-  "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3281188/#__sec30title";
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,38 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import { claimData, claimDataState } from "./claimData";
-import { queryStage, queryStageState } from "./queryStage";
-import { ents, entsState } from "./ents";
-import { params, paramsState } from "./params";
-import { evidence, evidenceState } from "./evidence";
-import { snackbar, snackbarState } from "./snackbar";
-import { docs, docsState } from "./docs";
-import { analysisData, analysisDataState } from "./analysisData";
-
 Vue.use(Vuex);
 
 export type State = {
-  snackbar: snackbarState;
-  claimData: claimDataState;
-  queryStage: queryStageState;
-  ents: entsState;
-  params: paramsState;
-  evidence: evidenceState;
-  docs: docsState;
-  analysisData: analysisDataState;
+  //
 };
 
 const store = new Vuex.Store<State>({
   modules: {
-    snackbar: snackbar,
-    claimData: claimData,
-    queryStage: queryStage,
-    ents: ents,
-    params: params,
-    evidence: evidence,
-    docs: docs,
-    analysisData: analysisData,
+    //
   },
 });
 
