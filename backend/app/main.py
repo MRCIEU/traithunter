55493acbc45724207bb279ca9773977dd331b905
YiLiu6240 <yi.liu6240@gmail.com>
2024-09-01 17:09:55 +0100

    backend: Use cors middleware in fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import dictionary, entity, utils, vector
 
